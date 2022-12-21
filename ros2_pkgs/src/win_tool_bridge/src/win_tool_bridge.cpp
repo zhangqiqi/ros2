c273a6b1dfbc90eefa8f4c999eb22a3fc9eed5c4
zhangqiqi <https://github.com/zhangqiqi/ros2_test_proj.git>
2022-12-21 23:57:28 +0800

    构建系统编译架构；建立组建、外设、ros节点的整体编译系统

--- ros2_pkgs/src/win_tool_bridge/src/win_tool_bridge.cpp
+++ ros2_pkgs/src/win_tool_bridge/src/win_tool_bridge.cpp
@@ -1,10 +1,35 @@
 #include <cstdio>
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+
+
+using namespace std::chrono_literals;
+
+class WinToolBridge : public rclcpp::Node
+{
+public:
+    WinToolBridge() : Node("win_tool_bridge") {
+        timer = this->create_wall_timer(500ms, std::bind(&WinToolBridge::timer_cb, this));
+    }
+private:
+    void timer_cb() {
+        RCLCPP_INFO(this->get_logger(), "timer start...");
+    }
+
+    rclcpp::TimerBase::SharedPtr timer;
+};
+
 
 int main(int argc, char ** argv)
 {
-  (void) argc;
-  (void) argv;
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<WinToolBridge>());
+    rclcpp::shutdown();
 
-  printf("hello world win_tool_bridge package\n");
-  return 0;
+    return 0;
 }
