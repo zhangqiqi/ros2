5bf119449991a2ce129ffeb91dedec4076f41504
zhangqiqi <790081738@qq.com>
2023-04-08 11:40:58 +0000

    上传lidar设备节点更新，增加设备数据接入、解析，laserscan消息构造等相关功能，已能够进行laser消息的发布，消息字段信息可能还需要调整

--- ros2_pkgs/src/lidar_reader/src/lidar_reader.cpp
+++ ros2_pkgs/src/lidar_reader/src/lidar_reader.cpp
@@ -4,10 +4,12 @@
 #include <rclcpp/logging.hpp>
 #include <rclcpp/callback_group.hpp>
 #include <rclcpp/executors/multi_threaded_executor.hpp>
+#include <rclcpp/publisher.hpp>
 #include <string>
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
 #include "lidar_dev.h"
 
 
@@ -25,20 +27,33 @@ public:
 		}
 
 		callback_group_reentrant = this->create_callback_group(rclcpp::CallbackGroupType::Reentrant);
-
-		timer_ = this->create_wall_timer(10ms, std::bind(&LidarReader::lidar_exec_timer, this));	
+		timer_ = this->create_wall_timer(2ms, std::bind(&LidarReader::lidar_exec_timer, this));	
+		lidar_pub = this->create_publisher<sensor_msgs::msg::LaserScan>("LaserScan", 10);
 	}
 private:
 	void lidar_exec_timer()
 	{
 		this->lidar_dev->exec();
+
+		do
+		{
+			auto msg = lidar_dev->get_next_msgs();
+			if (nullptr == msg)
+			{
+				break;
+			}
+			RCLCPP_DEBUG(get_logger(), "pub new lidar msg: angle(%f, %f), angle_increment(%f), time_increment(%f)",
+				msg->angle_min, msg->angle_max, msg->angle_increment, msg->time_increment
+			);
+			lidar_pub->publish(*msg);
+		} while (true);
 	}
 
 	rclcpp::CallbackGroup::SharedPtr callback_group_reentrant ;
 	rclcpp::TimerBase::SharedPtr timer_;
 	std::shared_ptr<LidarDev> lidar_dev;
+	rclcpp::Publisher<sensor_msgs::msg::LaserScan>::SharedPtr lidar_pub;
 };
-
 
 
 int main(int argc, char *argv[])
